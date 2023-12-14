9c19a421d0c30b35d42264142f51c866585c734a
vitthal-korvan <korvanvitthal@gmail.com>
2023-12-14 07:52:34 +0530

    14 DEC 2023

--- src/Timepass.java
+++ src/Timepass.java
@@ -4,29 +4,11 @@ public class Timepass {
 
 
     public static void main(String[] args) {
+        int a=1234543443;
+        int rem = (int)Math.log10(a)+1;
+        System.out.println(rem);
 
-        /* Your class should be named Solution.
-         * Read input as specified in the question.
-         * Print output as specified in the question.
-         */
-        Scanner sc = new Scanner(System.in);
+    }
 
-        int n= sc.nextInt();
-        System.out.println(countDigits(n));
-    }
-    public static int countDigits(int n){
-        // Write your code here.
-        int rem,count=0,temp=n;
-        while(temp>0){
-            rem=temp%10;
-            if(rem>0) {
-                if (n % rem == 0) {
-                    count++;
-                }
-            }
-            temp=temp/10;
-        }
-        return count;
-    }
 
 }
